074aec590bf95daa027d4aabc5657573127d510e
HuuHoang2707 <hoang.nh2707@gmail.com>
2024-04-05 23:34:28 +0700

    split code to detail

--- GUI.py
+++ GUI.py
@@ -1,25 +1,45 @@
-from tkinter import Tk, Frame, BOTH
+from tkinter import *
+import pickle
 
-class Example(Frame):
 
-    def __init__(self, parent):
+def Build_GUI(model, tfidf_vectorizer, filename):
+    pickle.dump(model, open(filename, 'wb'))
+    spam_model = pickle.load(open("finalized_model.sav",'rb'))
+    
+    def check_spam():
+        text = spam_text_Entry.get()
+        is_spam = spam_model.predict(tfidf_vectorizer.transform([text]))
+        if is_spam == 1:
+            print("text is spam")
+            my_string_var.set("Result: text is spam")
+        else:
+            print("text is not spam")
+            my_string_var.set("Result: text is not spam")
 
-        Frame.__init__(self, parent, background="white")
 
-        self.parent = parent
+    win = Tk()
+    
+    win.geometry("400x600")
+    win.configure(background="white")
+    win.title("Message/Comment Spam Detector")
+    
+    title = Label(win, text="Message/Comment Spam Detector", bg="gray",
+                  width="300",height="2",fg="white",
+                  font=("Calibri 20 bold italic underline")).pack()
+    
+    spam_text = Label(win, text="Enter your Text: ",bg="cyan",
+                       font=("Verdana 12")).place(x=12,y=100)
+    spam_text_Entry = Entry(win, textvariable=spam_text,width=33)
+    spam_text_Entry.place(x=155, y=105)
+    
+    my_string_var = StringVar()
+    my_string_var.set("Result: ")
+    
+    
+    print_spam = Label(win, textvariable=my_string_var,bg="cyan",
+                        font=("Verdana 12")).place(x=12,y=200)
 
-        self.initUI()
-
-    def initUI(self):
-
-        self.parent.title("Simple")
-
-        self.pack(fill=BOTH, expand=1)
-
-root = Tk()
-
-root.geometry("250x150+300+300")
-
-app = Example(root)
-
-root.mainloop()
+    button = Button(win, text="Submit",width="12",height="1",activebackground="red",bg="Pink",command=check_spam,font=("Verdana 12")).place(x=12,y=150)
+    
+    
+    win.mainloop()
